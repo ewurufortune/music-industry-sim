e84638a57549f730b1108b9012c1f7f889d7ef95
Ewuru Freeborn <109674539+15sxope@users.noreply.github.com>
2022-12-30 01:09:29 +0100

    finished...for now

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import Release from "./components/App";
+import TopRanker from './components/Chart';
+import AllTimer from './components/AllTimeChart'
+import Awards from './components/Awards.jsx'
+import AllArtistes from './components/AllArtists.jsx'
+import NewName from './components/NewArtist.jsx'
+import Date from './components/Date'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<div className="parent">
+ 
+ <div className="date">
+ <Date />
+
+    </div> 
+    <div className="div1">
+    <TopRanker />
+
+    </div>
+     <div className="div2">
+     {/* <AllTimer/> */}
+
+    </div>
+     <div className="div2">
+     {/* <Awards /> */}
+
+    </div>
+     <div className="div4">
+     {/* <AllArtistes /> */}
+
+    </div>
+     <div className="div5">
+     <p><i className="fa-brands fa-twitter fa-lg"></i>@FortuneEwuru</p>
+
+    </div>
+    <div className="div6">
+    {/* <Release /> */}
+
+    </div>
+    {/* <div className="newname">
+    <NewName /> 
+
+    </div> */}
+
+
+
+</div>);
+
